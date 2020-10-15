9cf7849334e8d2aa2e0403e900875e13e80d7cf4
sunchunhui <sunchunhui@aiyunxiao.com>
2020-10-15 15:26:31 +0800

    ADD: 初始化工程

---
+++ ImageLoader/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  ImageLoader
+//
+//  Created by Chunhui Sun on 2020/9/10.
+//  Copyright © 2020 YunXiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (nonatomic, strong) UIWindow *window;
+
+@end
+
---
+++ ImageLoader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImageLoader
+//
+//  Created by Chunhui Sun on 2020/9/10.
+//  Copyright © 2020 YunXiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
